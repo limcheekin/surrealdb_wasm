600ecf3665a4f27655fec6655e4b95c4ceb36744
Lim Chee Kin <limcheekin@vobject.com>
2023-10-04 14:14:10 +0800

    feat: updated example to include error handling

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:surrealdb_wasm/surrealdb_wasm.dart';
 import 'package:flutter_console_widget/flutter_console.dart';
 
 void main() {
+  //WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -34,87 +35,106 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final FlutterConsoleController controller = FlutterConsoleController();
   final db = Surreal();
+  static const commandSymbol = ">";
 
   Future<dynamic> execute(Function function, [String? message]) async {
     String functionString = function.toString();
+    final consoleMessage = message ??
+        functionString.substring(
+          functionString.indexOf("db."),
+        );
     controller.print(
-      message: message ??
-          functionString.substring(
-            functionString.indexOf("db."),
-          ),
+      message: "$commandSymbol $consoleMessage",
       endline: false,
     );
-    final result = await function();
-    controller.print(
-      message: " ✅",
-      endline: true,
-    );
-    if (result != null) {
-      debugPrint("homepage: result $result");
+    dynamic result;
+    try {
+      result = await function();
       controller.print(
-        message: "👀 $result",
+        message: " ✅",
+        endline: true,
+      );
+      if (result != null) {
+        debugPrint("homepage: result $result");
+        controller.print(
+          message: result.toString(),
+          endline: true,
+        );
+      }
+    } catch (error, stackTrace) {
+      controller.print(
+        message: " ❎ $error",
+        endline: true,
+      );
+      controller.print(
+        message: stackTrace.toString(),
         endline: true,
       );
     }
     return result;
   }
 
+  Future<void> executeSurrealDbCodes() async {
+    await execute(
+      () => db.connect("indxdb://surreal"),
+    );
+
+    await execute(
+      () => db.use(ns: "surreal", db: "surreal"),
+    );
+
+    final created = await execute(
+      () {
+        return db.create(
+          "person",
+          {
+            "title": "CTO",
+            "name": {
+              "first": "Tom",
+              "last": "Jerry",
+            },
+            "marketing": true,
+          },
+        );
+      },
+    );
+
+    await execute(
+      () => db.merge(
+        created['id'],
+        {
+          "marketing": false,
+        },
+      ),
+    );
+
+    await execute(
+      () => db.select("person"),
+    );
+
+    await execute(
+      () {
+        final groups = db.query(
+          "SELECT marketing, count() FROM type::table(\$table) GROUP BY marketing",
+          {
+            "table": "person",
+          },
+        );
+        return groups;
+      },
+    );
+
+    await execute(
+      () => db.delete("person"),
+    );
+  }
+
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) async {
-      await execute(
-        () => db.connect("indxdb://surreal"),
-      );
-
-      await execute(
-        () => db.use(ns: "surreal", db: "surreal"),
-      );
-
-      final created = await execute(
-        () {
-          return db.create(
-            "person",
-            {
-              "title": "CTO",
-              "name": {
-                "first": "Tom",
-                "last": "Jerry",
-              },
-              "marketing": true,
-            },
-          );
-        },
-      );
-
-      await execute(
-        () => db.merge(
-          created['id'],
-          {
-            "marketing": false,
-          },
-        ),
-      );
-
-      await execute(
-        () => db.select("person"),
-      );
-
-      await execute(
-        () {
-          final groups = db.query(
-            "SELECT marketing, count() FROM type::table(\$table) GROUP BY marketing",
-            {
-              "table": "person",
-            },
-          );
-          return groups;
-        },
-      );
-
-      await execute(
-        () => db.delete("person"),
-      );
+      await executeSurrealDbCodes();
+      echoLoop();
     });
   }
 
@@ -131,7 +151,6 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    echoLoop();
     final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
