10f932c2d5d133a519a0a01686270408370093ed
limcheekin <limcheekin@vobject.com>
2023-10-04 09:34:46 +0000

    Deploying to gh-pages from @ limcheekin/surrealdb_wasm@b4a71f94bd48a015b0ccbcd851e4f754bc4b0f74 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,9 +24,9 @@ const RESOURCES = {"canvaskit/chromium/canvaskit.js": "96ae916cd2d1b7320fff853ee
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "bbf3ea43dddb68a6c8a536cb01f7c73c",
-"/": "bbf3ea43dddb68a6c8a536cb01f7c73c",
-"main.dart.js": "c0b8d1e283e265ab672b5cc7de3368f7",
+"index.html": "aa787465eddbb1b7fdd7d98043d59d96",
+"/": "aa787465eddbb1b7fdd7d98043d59d96",
+"main.dart.js": "ea841319dd93e28f1bfe70e7d9d9bec2",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "manifest.json": "0867c3e13649ac4d06fe34b7b3ddce08"};
