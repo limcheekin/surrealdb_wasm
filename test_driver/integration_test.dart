129e3ab50ab29e14a109ad901a5d1b0d3c6de2c7
Lim Chee Kin <limcheekin@vobject.com>
2023-10-20 13:59:14 +0800

    feat: added integration test reports creation and commit to repo after test execution

--- test_driver/integration_test.dart
+++ test_driver/integration_test.dart
@@ -1,3 +1,10 @@
-import 'package:integration_test/integration_test_driver.dart';
+import 'package:integration_test/integration_test_driver.dart'
+    as integration_test_driver;
 
-Future<void> main() => integrationDriver();
+Future<void> main() async {
+  /// Repports are generated on given path, once all tested passed,
+  integration_test_driver.testOutputsDirectory = 'integration_test/reports';
+  await integration_test_driver.integrationDriver(
+    timeout: const Duration(minutes: 3),
+  );
+}
