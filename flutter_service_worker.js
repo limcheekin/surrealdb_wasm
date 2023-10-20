8ef66933371504dc2ad249a2b4b435c24cee0ce4
limcheekin <limcheekin@vobject.com>
2023-10-20 08:10:14 +0000

    Deploying to gh-pages from @ limcheekin/surrealdb_wasm@e9750d5a085ac64b127d07e988ce45292303fb1a 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "147fd7af9b5189c5504912f54887adda",
-"/": "147fd7af9b5189c5504912f54887adda",
+const RESOURCES = {"index.html": "ce9b134d59d0e597922a0ca4cab9e221",
+"/": "ce9b134d59d0e597922a0ca4cab9e221",
 "version.json": "10480ab669b41b140cc75519feb448ba",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "assets/AssetManifest.bin": "54dce800720257bf6b628c19115b377a",
