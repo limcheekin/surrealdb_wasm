b9f9eaaa1d6dce2bf2f25e7d6a97a62c3c148b30
Lim Chee Kin <limcheekin@vobject.com>
2023-10-22 12:16:14 +0800

    feat: clean up and rename test file

--- ci-integration-test.sh
+++ ci-integration-test.sh
@@ -1,1 +1,1 @@
-flutter drive --driver=test_driver/integration_test.dart --target integration_test/app_test.dart -d web-server --release --browser-name=chrome
+flutter drive --driver=test_driver/integration_test.dart --target integration_test/surrealdb_wasm_test.dart -d web-server --release --browser-name=chrome
