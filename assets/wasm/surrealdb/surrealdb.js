939c8207890bf1822881afd6ae1bfd1132132ceb
Lim Chee Kin <limcheekin@vobject.com>
2023-11-18 17:58:26 +0800

    feat: clean up wasm-flate related codes and files

--- assets/wasm/surrealdb/surrealdb.js
+++ assets/wasm/surrealdb/surrealdb.js
@@ -6,22 +6,12 @@ import * as fflate from "https://cdn.skypack.dev/fflate@0.8.0?min";
 
   const input = new URL("surrealdb.wasm.gz", import.meta.url);
   const imports = __wbg_get_imports();
-
   const response = await fetch(input);
 
   __wbg_init_memory(imports);
 
   const data = new Uint8Array(await response.arrayBuffer());
-
-  // Decompress the data using wasm-flate
-  //const flate = await wasm_bindgen(
-  //  "https://unpkg.com/wasm-flate/wasm_flate_bg.wasm"
-  //);
-
-  //const compressed_data = flate.gzip_encode_raw(data);
-  //console.log("compressed_data", compressed_data);
   const decompressed_data = fflate.gunzipSync(data);
-  //console.log("decompressed_data", decompressed_data);
   const bytes = decompressed_data.buffer;
 
   const { instance, module } = await WebAssembly.instantiate(bytes, imports);
@@ -31,19 +21,6 @@ import * as fflate from "https://cdn.skypack.dev/fflate@0.8.0?min";
   return __wbg_finalize_init(instance, module);
 })();
 
-// REF: https://stackoverflow.com/questions/45670597/wait-until-variable-equals
-//const interval = setInterval(waitForFlate, 100);
-
-//async function waitForFlate() {
-//  if (typeof flate !== "undefined") {
-// We don't need to interval the waitForFlate function anymore,
-// clearInterval will stop its periodical execution.
-//    clearInterval(interval);
-
-//await loadWasm();
-
-//  }
-//}
 class SurrealWrapper {
   /**
    * Construct the database engine
