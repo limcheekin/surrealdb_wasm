095c209edef8371838eedab52054ef52159e08ce
limcheekin <limcheekin@vobject.com>
2024-08-26 07:25:19 +0000

    Deploying to gh-pages from @ limcheekin/surrealdb_wasm@de17487c800325a2d35d687cd7ce76e122195aab 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"c9b9d5780da342eb3f0f5e439a7db06f7d1125
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4107933876"
+    serviceWorkerVersion: "1147410324"
   }
 });
