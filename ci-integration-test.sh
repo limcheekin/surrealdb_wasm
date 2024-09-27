1ba2ff961c2d88afed04dcb58af4f05bfd5cba80
limcheekin <limcheekin@vobject.com>
2024-09-27 13:53:40 +0800

    chore: update sh files to use surrealdb docker container

--- ci-integration-test.sh
+++ ci-integration-test.sh
@@ -1,3 +1,3 @@
-surreal start memory --allow-all --auth --user root --pass root &
+docker run --rm --pull always -p 8000:8000 --name surrealdb surrealdb/surrealdb:latest start memory -A --user root --pass root &
 flutter drive --driver=test_driver/integration_test.dart --target integration_test/all_tests.dart -d web-server --release --browser-name=chrome
-killall surreal
+docker container stop surrealdb
